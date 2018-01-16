dc283794d01ca08919fc7cfe83f13199d7aa0032
unknown <mtzcpd1119@DESK_M_D0-69.intra.cvc>
2018-01-16 21:23:20 -0200

    first commit

---
+++ looping_em_ruby.rb
@@ -0,0 +1,50 @@
+#While - enquanto - precisa de uma valvular de escape
+
+#valvula de escape
+index = 0
+#enquanto o que o index for menor que 5 imprima ele
+while index < 5
+    puts index
+    index +=1
+end
+#imprime de 0 a 4
+
+#enquanto o index for menor ou igual a 5 imprima ele
+index = 0
+while index <= 5
+    puts index
+    index +=1
+end
+#imprime de 0 a 5
+
+#while em uma linha só
+puts "oi"
+
+#até - ele é bem parecido com o while
+i = 0
+until i < 5 or i < 4
+    puts i 
+    i = i + 1
+end 
+
+
+#loop
+
+
+
+#times
+
+
+#for
+
+
+#each
+
+
+
+#map
+
+
+
+#select
+
